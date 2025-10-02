7dcdd7b44149cee71378910751a3d917ca09c51b
nvttles <leomb1012@icloud.com>
2025-10-02 18:45:02 -0400

    Create calc.clj

---
+++ src/nvttles/calc.clj
@@ -0,0 +1,17 @@
+;; Hello! This is a clojure calculator I made while I was learning clojure. It only has a few
+;; basic functions, such as 'add', 'subtract', 'multiply', 'divide'. So as you can see a very
+;; basic calculator.
+
+;; Made by nvttles (2025)
+
+(defn add [a b] 
+    (+ a b))
+
+(defn subtract [a b] 
+    (- a b))
+
+(defn multiply [a b] 
+    (* a b))
+
+(defn divide [a b] 
+    (/ a b))
